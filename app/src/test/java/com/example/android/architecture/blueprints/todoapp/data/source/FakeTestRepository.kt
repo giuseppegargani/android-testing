58a1995611fbd6880251433e575eaadb131aae7a
giuseppegargani <51164844+giuseppegargani@users.noreply.github.com>
2021-08-11 03:13:50 +0200

    Commit iniziale

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
@@ -10,10 +10,15 @@ import java.util.LinkedHashMap
 
 class FakeTestRepository: TasksRepository {
 
+    private var shouldReturnError = false
+
     var tasksServiceData: LinkedHashMap<String, Task> = LinkedHashMap()
 
     private val observableTasks = MutableLiveData<Result<List<Task>>>()
 
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 
     override suspend fun refreshTasks() {
         observableTasks.value = getTasks()
@@ -44,6 +49,9 @@ class FakeTestRepository: TasksRepository {
     }
 
     override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        if (shouldReturnError) {
+            return Result.Error(Exception("Test exception"))
+        }
         tasksServiceData[taskId]?.let {
             return Result.Success(it)
         }
@@ -51,6 +59,9 @@ class FakeTestRepository: TasksRepository {
     }
 
     override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
+        if (shouldReturnError) {
+            return Result.Error(Exception("Test exception"))
+        }
         return Result.Success(tasksServiceData.values.toList())
     }
 
